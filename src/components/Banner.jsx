b6a3659c6b1717bf25189e866b7a50122c9514c0
specialbrocoli <undertone252passinb.undertone252@passinbox.com>
2025-10-06 09:31:45 +0200

    feat: ✨ Add Layout component, includes Navbar/Footer wrapper feat: ✨ Add NotFound component, includes return-to-home button refactor: ♻️ Update Banner component, replace single quotes with double quotes refactor: ♻️ Update Navbar component, introduce NavLink for active styles style: 💄 Update Navbar styles, improve hover and active link transitions

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../components/Button";
 
-const Banner = ({ className = '' }) => {
+const Banner = ({ className = "" }) => {
   return (
-    <section className='p-2 lg:p-0'>
+    <section className="p-2 lg:p-0">
       {/* Banner container */}
-      <div className='rounded-xl overflow-hidden lg:rounded-none lg:rounded-t-xl lg:rounded-tr-xl'>
+      <div className="rounded-xl overflow-hidden lg:rounded-none lg:rounded-t-xl lg:rounded-tr-xl">
         {/* Banner background and content */}
         <div
           className={`h-[90vh] bg-[url("/banner-2.png")] bg-cover bg-center flex items-end justify-center relative after:absolute after:inset-0 after:bg-gradient-to-t after:from-black after:via-transparent after:to-transparent ${className}`}
         >
           {/* Section button */}
           <div>
-            <Button className='-mt-25 z-10'>
-              <Link to='/admissions'>Apply Now</Link>
+            <Button className="-mt-25 z-10">
+              <Link to="/admissions">Apply Now</Link>
             </Button>
           </div>
         </div>
