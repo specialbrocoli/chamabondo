a9df70fc0a7470ba470aff63abe52bbf5823c1b4
MikkieTee02 <mikaelakayz@gmail.com>
2025-10-14 10:25:54 +0200

    wrap app with observer provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import Home from './pages/Home';
-import About from './pages/About';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
-import Calender from './pages/Calender';
-import Projects from './pages/Projects';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Admissions from './pages/Admissions';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import Calender from "./pages/Calender";
+import Projects from "./pages/Projects";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Admissions from "./pages/Admissions";
+import { Route, Routes } from "react-router-dom";
+import ObserverProvider from "./components/Obeserver-provider";
+
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/Admissions' element={<Admissions />} />
-        <Route path='/Calender' element={<Calender />} />
-        <Route path='/Projects' element={<Projects />} />
-        <Route path='/Gallery' element={<Gallery />} />
-        <Route path='/Contact' element={<Contact />} />
-      </Routes>
-      <Footer />
+      <ObserverProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Admissions" element={<Admissions />} />
+          <Route path="/Calender" element={<Calender />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </ObserverProvider>
     </>
   );
 };
